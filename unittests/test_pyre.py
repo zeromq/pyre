5949103f4fba45e00f956651b99a7471a1dc1660
Rio Kierkels <riokierkels@gmail.com>
2014-12-05 15:10:53 +0100

    substituted assert methods with more appropriate ones

--- unittests/test_pyre.py
+++ unittests/test_pyre.py
@@ -15,54 +15,66 @@ class PyreTest(unittest.TestCase):
         #self.node2.set_name("node2")
         # give time for nodes to exchange
         #super(unittest.TestCase, self).__init__(*args, **kwargs)
+    # end setUp
 
     def tearDown(self):
         global inst_count
         if inst_count == 1:
             node1.stop()
             node2.stop()
-
-    def test_B(self):
-        self.assertTrue(True)
+    # end tearDown
 
     def test_get_name(self):
-        self.assertTrue("node1" == node1.get_name())
-        self.assertTrue("node2" == node2.get_name())
+        self.assertEqual("node1", node1.get_name())
+        self.assertEqual("node2", node2.get_name())
+    # end test_get_name
 
     def test_get_peers(self):
         id1 = node1.get_uuid()
         peers = node2.get_peers()
-        self.assertTrue(isinstance(peers, list))
-        self.assertTrue(id1 in peers)
+
+        self.assertIsInstance(peers, list)
+        self.assertIn(id1, peers)
+    # end test_get_peers
 
     def test_get_peer_address(self):
         id1 = node1.get_uuid()
         id2 = node2.get_uuid()
-        self.assertTrue(isinstance(node1.get_peer_address(id2), str))
-        self.assertTrue(isinstance(node2.get_peer_address(id1), str))
+
+        self.assertIsInstance(node1.get_peer_address(id2), str)
+        self.assertIsInstance(node2.get_peer_address(id1), str)
+    # end test_get_peer_address
 
     def test_get_peer_header_value(self):
         id1 = node1.get_uuid()
         id2 = node2.get_uuid()
-        self.assertTrue("1" == node1.get_peer_header_value(id2, "X-TEST"))
-        self.assertTrue("1" == node2.get_peer_header_value(id1, "X-TEST"))
+
+        self.assertEqual("1", node1.get_peer_header_value(id2, "X-TEST"))
+        self.assertEqual("1", node2.get_peer_header_value(id1, "X-TEST"))
+    # end test_get_peer_header_value
 
     def test_get_own_groups(self):
         node1.join("TEST")
         node2.join("TEST")
+
         # pyre works asynchronous so give some time to let changes disperse
         time.sleep(0.5)
-        self.assertTrue("TEST" in node1.get_own_groups())
-        self.assertTrue("TEST" in node2.get_own_groups())
+
+        self.assertIn("TEST", node1.get_own_groups())
+        self.assertIn("TEST", node2.get_own_groups())
+    # end test_get_own_groups
 
     def test_get_peer_groups(self):
-        self.assertTrue("TEST" in node1.get_peer_groups())
-        self.assertTrue("TEST" in node1.get_peer_groups())
+        self.assertIn("TEST", node1.get_peer_groups())
+        self.assertIn("TEST", node2.get_peer_groups())
+    # end test_get_peer_groups
 
     def test_zfinal(self):
         global inst_count
         inst_count = 1
         self.assertTrue(True)
+    # end test_zfinal
+# end PyreTest
 
 if __name__ == '__main__':
     inst_count = 0
