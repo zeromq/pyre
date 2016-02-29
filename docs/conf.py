2df43a09c96621a733d3271b3fe5558f4c9cfaf9
Arnaud Loonstra <arnaud@sphaero.org>
2016-02-29 15:39:21 +0100

    fix sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Pyre'
-copyright = '2014, Arnaud Loonstra'
+copyright = '2014, ZeroMQ community and its contributers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -259,3 +259,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+autodoc_mock_imports = [
+    'zmq',
+]
