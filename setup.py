376e055273296fa3c974272e3f26867b036f112b
GitHub Action <github-action@users.noreply.github.com>
2021-07-30 17:04:56 +0000

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
         name='zeromq-pyre',
-        version='0.3.3',
+        version='0.3.4',
         description='Python ZRE implementation',
         author='Arnaud Loonstra',
         author_email='arnaud@sphaero.org',
